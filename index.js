555e1f829136994a0526865fdf3e423154c47eb6
Your Name <you@example.com>
2023-12-02 08:16:23 +0600

    env

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Cars-House
-// BTmyjL4EaFZOsdVs
 
 
-
-const uri = "mongodb+srv://Cars-House:BTmyjL4EaFZOsdVs@cluster0.vylls0m.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vylls0m.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +27,9 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
