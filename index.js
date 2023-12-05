3064c1fa859ead6852ec31261a7e3f6c50c8ebd2
Your Name <you@example.com>
2023-12-05 20:30:12 +0600

    env

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
-
+require('dotenv').config()
+const port = process.env.PORT || 7000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('brand shop server is running');
+})
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vylls0m.mongodb.net/?retryWrites=true&w=majority`;
+// houseCar
+// ZjKP2co7mXRLPrL9
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://houseCar:ZjKP2co7mXRLPrL9@cluster0.xipfv.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xipfv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +38,6 @@ async function run() {
         client.connect();
 
         const carCollection = client.db('carDB').collection('car')
-        const userCollection = client.db("carDB").collection('user')
 
 
         app.get('/car', async (req, res) => {
@@ -38,12 +46,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/car/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await carCollection.findOne(query)
-            res.send(result)
-        })
+        // 1 step
         app.post('/car', async (req, res) => {
             const newCar = req.body;
             console.log(newCar);
@@ -51,34 +54,8 @@ async function run() {
             res.send(result)
         })
 
-        app.put('/car/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true }
-            const updateCar = req.body;
-            const car = {
-                $set: {
-                    photo: updateCar.photo,
-                    name: updateCar.name,
-                    brand: updateCar.brand,
-                    price: updateCar.price,
-                    rating: updateCar.rating,
-                    type: updateCar.type
-                }
-            }
-            const result = await carCollection.updateOne(filter, car, options)
-            res.send(result)
-        })
 
 
-        // user related apis
-
-        // app.post('/user', async (req, res) => {
-        //     const user = req.body;
-        //     console.log(user)
-        //     const result = await userCollection.insertOne(user)
-        //     res.send(result)
-        // })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -92,10 +69,8 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) => {
-    res.send('brand shop server is running');
-});
+
 
 app.listen(port, () => {
-    console.log(`brand shop server is running on port:${port}`)
+    console.log(`brand shop server is running on port: ${port}`)
 })
